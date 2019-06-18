e6bdb6d768a5de597f4ae20ecc1e43d7e2b38095
Maurice <mauricepoirrier@gmail.com>
2019-06-18 14:50:59 -0400

    issue_12 update

--- app/app.py
+++ app/app.py
@@ -4,29 +4,31 @@ from views.model import Model
 from os.path import join, dirname
 from os import getenv
 from dotenv import load_dotenv
-from utils import has_dependencies
+from utils import has_dependencies, create_directory
 
 dotenv_path = join(dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path)
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = getenv('UPLOAD_DIRECTORY', None)
+app.config['UPLOAD_FOLDER'] = getenv('UPLOAD_DIRECTORY', 'app/persistent/')
+create_directory(app.config['UPLOAD_FOLDER'])
 
 @app.route('/api/upload', methods=['POST'])
 def upload_image():
     if request.method == 'POST':
-        if not has_dependencies(request):
-            abort(400)
+        if not has_dependencies(request, True):
+            return Response("Wrong query", status=400)
         image_handler = ImageSaver()
         image = request.files['image']
         token = request.values['token']
-        return image_handler.save_image(image, token)
+        label = request.values['label']
+        return image_handler.save_image(image, token, label)
 
 @app.route('/api/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        if not has_dependencies(request):
-            abort(400)
+        if not has_dependencies(request, False):
+            return Response("Wrong query", status=400)
         model = Model()
         image_handler = ImageSaver()
         image = request.files['image']
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,7 @@
+import os
 
-def has_dependencies(request):
+
+def has_dependencies(request, is_labeled):
     '''
     Check if request has dependencies
     '''
@@ -7,4 +9,12 @@ def has_dependencies(request):
         return False
     elif request.values['token'] == "":
         return False
+    if is_labeled:
+        if 'label' not in request.values:
+            return False
     return True
+
+def create_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    
